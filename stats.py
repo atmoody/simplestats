c9189a275d532260a35913792a94f1482880d949
allison-moody <atmoody@gmail.com>
2016-01-15 13:48:54 -0600

    Took error checking functions out of file

--- stats.py
+++ stats.py
@@ -8,13 +8,7 @@ def mean(vals):
 	else:
 		return total/length
 	
-def test_mean():
-	assert mean([2,4]) == 3.0 #you know the answer you should get and you're testing the formula that it gives you the correct answer
-test_mean()
 
-def test_empty_list():
-	assert mean([]) == 0.0
-test_empty_list()
 
 ##assertions and exceptions make sure you're using the correct file type
 #runs program with non-numbers
