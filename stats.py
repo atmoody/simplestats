99e311e6988b96dad16f78a562a6721e8225f73b
allison-moody <atmoody@gmail.com>
2016-01-15 13:40:57 -0600

    Added empty list error test within function

--- stats.py
+++ stats.py
@@ -3,11 +3,18 @@ def mean(vals):
 	assert type(vals) is list, 'wrong input format'
 	total = sum(vals)
 	length = len(vals)
-	return total/length
+	if length == 0:
+		return 0.0
+	else:
+		return total/length
 	
 def test_mean():
 	assert mean([2,4]) == 3.0 #you know the answer you should get and you're testing the formula that it gives you the correct answer
 test_mean()
+
+def test_empty_list():
+	assert mean([]) == 0.0
+test_empty_list()
 
 ##assertions and exceptions make sure you're using the correct file type
 #runs program with non-numbers
