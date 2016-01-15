c2d79506db1a86fdf1026175d6c445f4350db99b
allison-moody <atmoody@gmail.com>
2016-01-15 14:23:52 -0600

    Added string list error check

--- stats.py
+++ stats.py
@@ -1,6 +1,7 @@
 def mean(vals):
 	"""Calculate the arithmetic mean of a list of numbers in vals"""
 	assert type(vals) is list, 'wrong input format'
+	vals = [float(x) for x in vals]
 	total = sum(vals)
 	length = len(vals)
 	if length == 0:
