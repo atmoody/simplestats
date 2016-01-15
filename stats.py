575c29bdc9d5ad3332a71a7114fd048d8bbfd5a4
allison-moody <atmoody@gmail.com>
2016-01-15 13:22:28 -0600

    Added assert error test

--- stats.py
+++ stats.py
@@ -1,7 +1,12 @@
 def mean(vals):
-    """Calculate the arithmetic mean of a list of numbers in vals"""
-    total = sum(vals)
-    length = len(vals)
-    return total/length
+	"""Calculate the arithmetic mean of a list of numbers in vals"""
+	assert type(vals) is list, 'wrong input format'
+	total = sum(vals)
+	length = len(vals)
+	return total/length
 	
-print(mean([2,4]))
+#print(mean([2,4]))
+
+##assertions and exceptions make sure you're using the correct file type
+#runs program with non-numbers
+print(mean("hello"))
